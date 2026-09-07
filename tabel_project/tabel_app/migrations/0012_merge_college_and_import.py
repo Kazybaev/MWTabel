4d3a7ac480128b59ce73a166dcbc5d0ac5b9d8e3
Alymbaek <alymbekibragimov46@gmail.com>
2026-09-07 17:09:26 +0000

    Merge college and import migrations

---
+++ tabel_project/tabel_app/migrations/0012_merge_college_and_import.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("tabel_app", "0011_alter_studentprofile_parent_name_importedgradecell"),
+        ("tabel_app", "0011_collegegroup_alter_studentprofile_parent_name_and_more"),
+    ]
+
+    operations = []
